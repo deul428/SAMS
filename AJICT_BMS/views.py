f087a32eb3061c2fca47eaa039294b06757a1826
cci10000 <anarchist10000@gmail.com>
2025-01-17 16:44:30 +0900

    f_select_biz_opp2()

--- AJICT_BMS/views.py
+++ AJICT_BMS/views.py
@@ -681,7 +681,7 @@ def f_select_biz_opp2(request):
                print(f"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
 
-               v_data = [{'Row': '0'}]
+               v_data = [{"Row": "0"}]
                v_status = {"STATUS":"NONE","MESSAGE":"Data가 존재하지 않습니다."}
 
 
@@ -689,7 +689,7 @@ def f_select_biz_opp2(request):
             print(f"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
 
 
-            return JsonResponse({"row":v_data,"status":v_status},safe = False,json_dumps_params = {'ensure_ascii':False})
+            return JsonResponse({"data":v_data,"status":v_status},safe = False,json_dumps_params = {'ensure_ascii':False})
       except DatabaseError:
          v_return = {'STATUS':'FAIL','MESSAGE':'DB에서 오류가 발생했습니다.'}
          v_square_bracket_return = [v_return]

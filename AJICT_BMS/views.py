3904b8e04879a1f12cfc6c92e45dfb83de9688a0
cci10000 <anarchist10000@gmail.com>
2025-01-17 16:17:25 +0900

    f_select_biz_opp2()

--- AJICT_BMS/views.py
+++ AJICT_BMS/views.py
@@ -675,6 +675,12 @@ def f_select_biz_opp2(request):
                v_status = {'STATUS':'NONE','MESSAGE':'Data가 존재하지 않습니다.'}
             else:
                v_status = {"STATUS":"SUCCESS","MESSAGE":"조회되었습니다!!!"}
+
+
+               #test
+               print(f"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+
+
             return JsonResponse({"data":v_data,"status":v_status},safe = False,json_dumps_params = {'ensure_ascii':False})
       except DatabaseError:
          v_return = {'STATUS':'FAIL','MESSAGE':'DB에서 오류가 발생했습니다.'}

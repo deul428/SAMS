1973627bf69e7c49fa0a8e6541ad6f2434ae0902
cci10000 <anarchist10000@gmail.com>
2025-01-17 15:52:11 +0900

    f_select_biz_opp2()

--- AJICT_BMS/views.py
+++ AJICT_BMS/views.py
@@ -670,7 +670,7 @@ def f_select_biz_opp2(request):
             print(f"f_select_biz_opp2()에서의 v_data : {v_data}")
 
 
-            if not v_data:
+            if not v_data or not isinstance(v_data,list):
                v_status = {'STATUS':'NONE','MESSAGE':'Data가 존재하지 않습니다.'}
             else:
                v_status = {"STATUS":"SUCCESS","MESSAGE":"조회되었습니다."}

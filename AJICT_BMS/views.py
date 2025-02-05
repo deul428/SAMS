ad2fecbbf0dc0ac4404b9e71b3bfb633a25c7c34
cci10000 <anarchist10000@gmail.com>
2025-02-05 15:35:15 +0900

    user_id

--- AJICT_BMS/views.py
+++ AJICT_BMS/views.py
@@ -897,7 +897,8 @@ def f_insert_biz_opp(request):
             v_change_preparation_dept_id = ''
             v_user_id = ''
             if v_auth1_code == 'AUT' and v_auth2_code == '0001':
-               v_user_name = None if v_body.get('biz_opp_detail',{}).get('a_user_name') == '' else v_body.get('biz_opp_detail',{}).get('a_user_name')
+               #v_user_name = None if v_body.get('biz_opp_detail',{}).get('a_user_name') == '' else v_body.get('biz_opp_detail',{}).get('a_user_name')
+               v_user_name = None if v_body.get('a_user_name') == '' else v_body.get('a_user_name')
                if v_user_name is not None:
                   v_user_name = v_user_name.strip()
                if not v_user_name:
@@ -1293,27 +1294,22 @@ def f_renewal_biz_opp(request):
             if v_biz_opp_detail:
                v_param = []
                v_set_clauses = []
-
-
-               #test
-               # v_user_name = None if v_body.get('a_user_name') == '' else v_body.get('a_user_name')
-               # if v_user_name is not None:
-               #    v_user_name = v_user_name.strip()
-               # if not v_user_name:
-               #    transaction.set_rollback(True)
-               #    v_return = {'STATUS':'FAIL','MESSAGE':"'담당자' 항목은 필수 전달 항목입니다!"}
-               #    v_square_bracket_return = [v_return]
-               #    return JsonResponse(v_square_bracket_return,safe = False,json_dumps_params = {'ensure_ascii':False})
-               # else:
-               #    v_sql_user = """SELECT user_id FROM ajict_bms_schema.aj_user WHERE user_name = %s AND delete_date IS NULL"""
-               #    v_param_select_user = []
-               #    v_param_select_user.append(v_user_name)
-               #    with connection.cursor() as v_cursor:
-               #       v_cursor.execute(v_sql_user,v_param_select_user)
-               #       v_row1 = v_cursor.fetchone()
-               #       v_user_id = v_row1[0]
-
-
+               v_user_name = None if v_body.get('a_user_name') == '' else v_body.get('a_user_name')
+               if v_user_name is not None:
+                  v_user_name = v_user_name.strip()
+               if not v_user_name:
+                  transaction.set_rollback(True)
+                  v_return = {'STATUS':'FAIL','MESSAGE':"'담당자' 항목은 필수 전달 항목입니다!"}
+                  v_square_bracket_return = [v_return]
+                  return JsonResponse(v_square_bracket_return,safe = False,json_dumps_params = {'ensure_ascii':False})
+               else:
+                  v_sql_user = """SELECT user_id FROM ajict_bms_schema.aj_user WHERE user_name = %s AND delete_date IS NULL"""
+                  v_param_select_user = []
+                  v_param_select_user.append(v_user_name)
+                  with connection.cursor() as v_cursor:
+                     v_cursor.execute(v_sql_user,v_param_select_user)
+                     v_row1 = v_cursor.fetchone()
+                     v_user_id = v_row1[0]
                for v_key,v_value in v_body.items():
                   if v_key == 'biz_opp_detail' and isinstance(v_value,dict):
                      for v_nested_key,v_nested_value in v_value.items():
@@ -1328,9 +1324,9 @@ def f_renewal_biz_opp(request):
                         v_set_clauses_biz_opp_detail_history.append(v_nested_key)
                         v_param.append(v_nested_value)
                v_sql_update_biz_opp_detail = f"UPDATE ajict_bms_schema.biz_opp_detail\
-                                               SET " + ",".join(v_set_clauses) + ",update_user = %s,update_date = CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Seoul'\
+                                               SET " + ",".join(v_set_clauses) + ",user_id = %s,update_user = %s,update_date = CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Seoul'\
                                                WHERE biz_opp_id = %s AND detail_no = %s"
-               # v_param.append(v_user_id)
+               v_param.append(v_user_id)
                v_param.append(v_session_user_id)
                v_param.append(v_biz_opp_id)
                v_param.append(v_detail_no)

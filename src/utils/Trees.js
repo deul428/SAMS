c54109e5e62f2155f7470db54167e9eb4ea57a4f
kimhs <kimhs@ajnet.co.kr>
2025-02-07 11:29:19 +0900

    kimhs 25.02.07. 사업 (기회) 등록/수정 매출 상세 관리 트리 UI 작업 중

--- src/utils/Trees.js
+++ src/utils/Trees.js
@@ -6,17 +6,36 @@ import { Form, FloatingLabel } from "react-bootstrap";
 import Tree from 'rc-tree';
 import 'rc-tree/assets/index.css';
 import '../styles/_tree.scss';
+import '../styles/_button.scss';
+
 const Trees = ({ v_treeName, show, onHide, data }) => {
     const [v_handlingHtml, setVHandlingHtml] = useState(null);
     const [treeData, setTreeData] = useState([]);
 
     let bizCode, corCode, priCode;
+    const switchData = (switchName) => {
+        console.log(switchName);
+    }
     useEffect(() => {
         if (data) {
             bizCode = data.data.search_biz_section_code;
             corCode = data.data.search_last_client_com_code;
             priCode = data.data.search_principal_product_code;
-            // console.log(bizCode, corCode, priCode);
+            console.log(bizCode, corCode, priCode);
+            const bizData = bizCode.map((e, index) => ({
+                title: e.small_classi_name,
+                key: `0-${index}`, // 키를 유니크하게 변경
+                children: [
+                    {
+                        title: 'Level 2 - Node 1',
+                        key: `0-${index}-0`,
+                        children: [
+                            { title: 'Level 3 - Node 1 (isLeaf)', key: `0-${index}-0-0`, isLeaf: true },
+                            { title: 'Level 3 - Node 2 (disabled)', key: `0-${index}-0-1`, disabled: true },
+                        ],
+                    }
+                ],
+            }));
             const newTreeData = bizCode.map((e, index) => ({
                 title: e.small_classi_name,
                 key: `0-${index}`, // 키를 유니크하게 변경
@@ -36,25 +55,6 @@ const Trees = ({ v_treeName, show, onHide, data }) => {
         }
     }, [data]);
     
-    
-    // bizCode.map((e) => {
-    //     console.log(e.small_classi_name);
-    //     const treeData = {
-    //         // title: e.small_classi_name,
-    //         key: '0-0',
-    //         children: [
-    //             {
-    //                 title: 'Level 2 - Node 1',
-    //                 key: '0-0-0',
-    //                 children: [
-    //                     { title: 'Level 3 - Node 1 (isLeaf)', key: '0-0-0-0', isLeaf: true },
-    //                     { title: 'Level 3 - Node 2 (disabled)', key: '0-0-0-1', disabled: true},
-    //                 ],
-    //             }
-    //         ],
-    //     }
-    // })
-    
     useEffect(() => {
         const updateUI = () => { 
             if (data) {
@@ -70,6 +70,14 @@ const Trees = ({ v_treeName, show, onHide, data }) => {
                                 <div className='modalcntnt'>
                                     <div className="inputField">
                                         <h3>{data.data.search_biz_section_code[0].great_classi_name}</h3>
+                                        <div className="btnArea treeBtnArea">
+                                            <Button variant="secondary" onClick={(e) => switchData(data.data.search_biz_section_code[0].great_classi_name)}>
+                                                {data.data.search_biz_section_code[0].great_classi_name}
+                                            </Button>
+                                            <Button variant="secondary" onClick={(e) => switchData(data.data.search_last_client_com_code[0].great_classi_name)}>
+                                                {data.data.search_last_client_com_code[0].great_classi_name}
+                                            </Button>
+                                        </div>
                                         <div className="searchItem">
                                             <FloatingLabel label='검색' controlId="floatingInput">
                                                 <Form.Control size='sm' type='text' placeholder="검색" />
@@ -77,7 +85,6 @@ const Trees = ({ v_treeName, show, onHide, data }) => {
                                             <Button variant='info'>조회</Button>
                                         </div>
                                         <Tree treeData={treeData} />
-                                        <Tree /* treeData={data} */ />
                                     </div>
                                 </div>
                             </Modal.Body>
